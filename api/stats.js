3a6f49ac8bfd3cbdb478ffbbf9a8580d75f93e1e
aventurasbiblicas89-design <aventurasbiblicas89@gmail.com>
2026-09-12 19:46:15 -0300

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -1,11 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  try {
-    const { Redis } = require('@upstash/redis');
-    const redis = Redis.fromEnv();
-    const stats = await redis.get('podio:stats') || { total: 0 };
-    return res.status(200).json(stats);
-  } catch (e) {
-    return res.status(200).json({ total: 0 });
-  }
-};
+  return res.status(200).json({ total: 0 });
+}
