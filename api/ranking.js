88f02f685ff2562952df97f985db2bba847689e3
aventurasbiblicas89-design <aventurasbiblicas89@gmail.com>
2026-09-12 19:45:24 -0300

    Update ranking.js

--- api/ranking.js
+++ api/ranking.js
@@ -1,11 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  try {
-    const { Redis } = require('@upstash/redis');
-    const redis = Redis.fromEnv();
-    const data = await redis.get('podio:ranking') || [];
-    return res.status(200).json(data);
-  } catch (e) {
-    return res.status(200).json([]);
-  }
-};
+  return res.status(200).json([]);
+}
