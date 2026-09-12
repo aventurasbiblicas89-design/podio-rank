0d55c033245d8b8cdc3eb2d8e8a9e85884c03fae
aventurasbiblicas89-design <aventurasbiblicas89@gmail.com>
2026-09-12 16:14:40 -0300

    Delete api/zerar.js

--- api/zerar.js
+++
@@ -1,8 +0,0 @@
-import { Redis } from '@upstash/redis';
-const redis = Redis.fromEnv();
-export default async function handler(req,res){
-  res.setHeader('Cache-Control','no-store');
-  const keys = await redis.keys('ranking:*');
-  for(const k of keys){ await redis.del(k); }
-  res.json({ok:true, apagados: keys});
-}
