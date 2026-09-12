46c7ea85d457ae87754e7c8b5ee374025c5da6b2
aventurasbiblicas89-design <aventurasbiblicas89@gmail.com>
2026-09-12 20:15:15 -0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,11 +1,4 @@
-module.exports = async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  if (req.method !== 'POST') return res.status(200).json({ ok: true });
-  try {
-    console.log('webhook', req.body);
-    return res.status(200).json({ ok: true });
-  } catch (e) {
-    return res.status(200).json({ ok: true });
-  }
-};
+export default async function handler(req, res) {
+  return res.json({ok: true});
+}
 
