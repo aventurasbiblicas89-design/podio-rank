3a1e8f413cc815f057c02e878f410cc1269c8afd
aventurasbiblicas89-design <aventurasbiblicas89@gmail.com>
2026-09-11 22:05:04 -0300

    Create fix-link.js

---
+++ api/fix-link.js
@@ -0,0 +1,13 @@
+import { Redis } from '@upstash/redis';
+const redis = Redis.fromEnv();
+export default async function handler(req,res){
+  let rank = await redis.get('ranking:marketing');
+  if(typeof rank==='string') rank=JSON.parse(rank);
+
+  rank[10].nome = "PEDRO HENRIQUE";
+  rank[10].url = "https://www.youtube.com/@Meme-zinho";
+  rank[10].clicks = 0;
+
+  await redis.set('ranking:marketing', JSON.stringify(rank));
+  res.json({ok:true, agora: rank[10]});
+}
